fb6be279758ebaaeaf2adb8e93941cd454bc063b
Edgar Medina <medient@hotmail.com>
2017-02-19 13:31:25 -0500

    Initial commit

---
+++ src/main/java/com/monkeds/init/Init.java
@@ -0,0 +1,11 @@
+package com.monkeds.init;
+
+/**
+ * Created by root on 2/19/17.
+ */
+public class Init {
+
+    public static void main (String [] args){
+        System.out.println("HOLA OCA");
+    }
+}
